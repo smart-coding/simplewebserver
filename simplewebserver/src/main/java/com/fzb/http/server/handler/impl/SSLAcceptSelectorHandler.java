7ecbdab41dd8efe0c443528adc5d4675f79e2484
xiaochun <xchun90@163.com>
2016-04-28 21:59:09 +0800

    add serverInfo

---
+++ simplewebserver/src/main/java/com/fzb/http/server/handler/impl/SSLAcceptSelectorHandler.java
@@ -0,0 +1,10 @@
+package com.fzb.http.server.handler.impl;
+
+import com.fzb.http.server.handler.api.AcceptSelectorHandler;
+
+public class SSLAcceptSelectorHandler implements AcceptSelectorHandler {
+    @Override
+    public void handleAccept() {
+
+    }
+}
